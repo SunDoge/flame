3f6addf03fa1c5a998c8bf520cbcd0181660909a
SunDoge <384813529@qq.com>
2021-04-14 17:29:21 +0800

    update metrics init

---
+++ flame/pytorch/metrics/__init__.py
@@ -0,0 +1,5 @@
+from .base_metric import Metric, MetricList
+from .batch_size_metric import BatchSizeMetric
+from .copy_metric import CopyMetric
+from .loss_metric import LossMetric
+from .topk_accuracy import TopkAccuracy
