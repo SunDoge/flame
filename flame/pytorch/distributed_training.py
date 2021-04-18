4e37ead7cf9ad3303229564c3e65081e27b36ac2
SunDoge <384813529@qq.com>
2021-04-18 11:38:55 +0800

    update dist training

--- flame/pytorch/distributed_training.py
+++ flame/pytorch/distributed_training.py
@@ -6,6 +6,9 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 from dataclasses import dataclass
 import flame
+import argparse
+import functools
+from flame.utils.experiment import get_experiment_dir, make_experiment_dir
 
 _logger = logging.getLogger(__name__)
 
@@ -15,10 +18,31 @@ class DistOptions:
     rank_start: int = 0
     world_size: int = 1
     dist_backend: str = 'NCCL'
-    dist_url: str = 'tcp://127.0.0.1:12345'
+    dist_host: str = '127.0.0.1'
+    dist_port: int = 12345
+
+    @property
+    def dist_url(self) -> str:
+        return 'tcp://{host}:{port}'.format(host=self.dist_host, port=self.dist_port)
+
+    def get_rank(self, proc_id: int) -> int:
+        return self.rank_start + proc_id
 
 
-def _init_process_group_fn(device_id: int, worker_fn: Callable, dist_options: DistOptions, *args):
+def get_dist_options() -> DistOptions:
+    parser = argparse.ArgumentParser(prog='distributed launcher')
+    parser.add_argument('--rank-start', type=int, default=0)
+    parser.add_argument('--world-size', type=int, default=1)
+    parser.add_argument('--dist-backend', type=str, default='nccl')
+    parser.add_argument('--dist-host', type=str, default='127.0.0.1')
+    parser.add_argument('--dist-port', type=int, default=12345)
+
+    args, _ = parser.parse_known_args()
+    dist_options = DistOptions(args.__dict__)
+    return dist_options
+
+
+def _init_process_group_fn(proc_id: int, worker_fn: Callable, dist_options: DistOptions, *args):
     """wrapper function for worker_fn
 
     必须定义成可以被pickle的函数。
@@ -30,7 +54,7 @@ def _init_process_group_fn(device_id: int, worker_fn: Callable, dist_options: Di
 
     """
 
-    rank = dist_options.rank_start + device_id
+    rank = dist_options.get_rank(proc_id)
     print(f'=> rank: {rank}')
 
     dist.init_process_group(
@@ -41,20 +65,17 @@ def _init_process_group_fn(device_id: int, worker_fn: Callable, dist_options: Di
     )
 
     if torch_cuda.is_available():
-        _logger.info('set cuda_device=%d', device_id)
-        torch_cuda.set_device(device_id)
+        _logger.info('set cuda_device=%d', proc_id)
+        torch_cuda.set_device(proc_id)
 
     worker_fn(*args)
 
 
 def start_distributed_training(
     worker_fn: Callable,
+    dist_options: DistOptions,
     args: tuple = (),
-    rank_start: int = 0,
-    world_size: int = 1,
-    dist_backend: str = 'NCCL',
-    dist_url: str = 'tcp://127.0.0.1:12345',
-    nprocs: int = 1,
+    nprocs: Optional[int] = None,
 ):
     """helper function for distributed training
 
@@ -65,25 +86,24 @@ def start_distributed_training(
 
     """
 
-    num_gpus = torch_cuda.device_count()
-
-    if num_gpus == 0 and dist_backend.upper() == 'NCCL':
-        _logger.error('no GPUs, stop training')
-        return
-
     # CPU + GLOO, nprocs = 1
     # nprocs = num_gpus if num_gpus > 0 else 1
 
-    dist_options = DistOptions(
-        rank_start=rank_start,
-        world_size=world_size,
-        dist_backend=dist_backend,
-        dist_url=dist_url,
-    )
+    if nprocs is None:
+        _logger.info('nprocs is None, start inferring nprocs')
+        if dist_options.dist_backend.lower() == 'nccl':
+            nprocs = torch_cuda.device_count()
+            if nprocs == 0:
+                _logger.error('no gpu for distributed training, stop')
+                return
+        else:
+            nprocs = 1
+
+        _logger.info('nprocs = %d', nprocs)
 
     mp.spawn(
         _init_process_group_fn,
-        args=(worker_fn, dist_options, *args),
+        args=(worker_fn, dist_options, args),
         nprocs=nprocs
     )
 
