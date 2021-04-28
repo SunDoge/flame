4e747fc560421426961a26b7d9eb831d40ea6257
SunDoge <384813529@qq.com>
2021-04-28 17:45:59 +0800

    fix ckcfg

--- flame/ckcfg.py
+++ flame/ckcfg.py
@@ -8,17 +8,17 @@
 """
 
 
-from .argument import parse_basic_args, BasicArgs
-from .config import config_snippet, from_snippet
 import rich
+
+from .argument import BasicArgs
+from .config import config_snippet, from_snippet
 
 
 def main():
-    args = parse_basic_args()
+    args = BasicArgs.from_args()
     snippet = config_snippet(args.local, args.config)
     cfg = from_snippet(snippet)
     rich.print(cfg)
-
 
 
 if __name__ == '__main__':
