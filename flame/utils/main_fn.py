3091a0c510441d80959dc9d8d2bd1706df68bc0d
SunDoge <384813529@qq.com>
2021-05-26 11:35:00 +0800

    add comments

--- flame/utils/main_fn.py
+++ flame/utils/main_fn.py
@@ -5,8 +5,10 @@ T = TypeVar('T')
 
 
 def main_fn(func: T) -> T:
+    # 拿到调用这个函数的 FrameInfo
     caller = inspect.stack()[1]
 
+    # 拿到 __name__
     name = caller.frame.f_globals['__name__']
 
     if name == '__main__':
