e0a285b9710796a9548e652f3e49f6516979cfcb
SunDoge <384813529@qq.com>
2021-10-05 23:34:51 +0800

    clean up

--- flame/next_version/logging.py
+++
@@ -1,1 +0,0 @@
-from flame.logging import *
--- flame/next_version/symbols.py
+++
@@ -1,7 +0,0 @@
-from typing import NewType
-from flame.next_version.arguments import BaseArgs
-from torch import nn
-
-IConfig = NewType('IConfig', dict)
-IArgs = NewType('IArgs', BaseArgs)
-IModel = NewType('IModel', nn.parallel.DistributedDataParallel)
