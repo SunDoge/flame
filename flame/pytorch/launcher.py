7c3be22ea4f23fc70d2f2c332a2fd2187f025d2c
SunDoge <384813529@qq.com>
2022-02-28 15:42:17 +0800

    fix logging

--- flame/pytorch/launcher.py
+++ flame/pytorch/launcher.py
@@ -2,7 +2,7 @@ from flame.pytorch.helpers.cudnn import cudnn_benchmark_if_possible
 from .arguments import BaseArgs
 from typing import Callable, Optional
 from flame.core.config_parser import ConfigParser
-from flame.core.logging import init_logging, log_runtime_env
+from flame.core.logging import init_logging, log_runtime_env, remove_all_handlers
 import torch.multiprocessing as mp
 import logging
 
@@ -34,12 +34,15 @@ def _init_distributed(
     else:
         # Use share file by default
         rank = args.init_process_group_from_file(local_rank)
-        
+
     if rank == 0:
         init_logging(
             filename=args.experiment_dir / 'experiment.log',
             debug=args.debug,
         )
+    else:
+        # 不确定是否应该这样做
+        remove_all_handlers()
 
     log_runtime_env()
 
